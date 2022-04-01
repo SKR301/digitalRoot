ae27340c5a0368e8059b096c20ff13beb79b31ee
SKR301 <chunnu.bihat@gmail.com>
2022-04-01 22:09:41 +0530

    made more robust with more tests

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
     return[int] : digital root on `num`    
 */
 function digitalRoot(num){
-    return num % 9;
+    if ((num % 1 !== 0) || (num < 0) || (typeof(num) !== 'number')) {
+        return 'Invalid input. Input must be a whole number.';
+    }
+
+    if(num == 0){
+        return 0;
+    }
+
+    return (num % 9 == 0) ? 9 : (num % 9);
 }
 
 module.exports = digitalRoot;
--- test/digital-root.test.js
+++ test/digital-root.test.js
@@ -1,5 +1,20 @@
 const digitalRoot = require('../index');
 
-test('checks digital root for a valid input', () => {
+test('checks for 0', () => {
+    expect(digitalRoot(0)).toBe(0);
+});
+test('checks for a valid input', () => {
     expect(digitalRoot(123)).toBe(6);
+});
+test('checks for a valid input which adds upto 9', () => {
+    expect(digitalRoot(1233)).toBe(9);
+});
+test('checks for string as input', () => {
+    expect(digitalRoot('123')).toBe('Invalid input. Input must be a whole number.');
+});
+test('checks for negative number as input', () => {
+    expect(digitalRoot(-123)).toBe('Invalid input. Input must be a whole number.');
+});
+test('checks for decimal number as input', () => {
+    expect(digitalRoot(12.3)).toBe('Invalid input. Input must be a whole number.');
 });
